8b265eb4adbd91d1a9fa304c7dd6926eb7ade9f0
Gabriel Fronchak <gabrielgmack@gmail.com>
2022-05-02 00:49:43 -0300

    Done add(element, position)

--- src/application/ProgramArray.java
+++ src/application/ProgramArray.java
@@ -10,7 +10,12 @@ public class ProgramArray {
 			Array array = new Array(10);
 			array.add("Gabriel");
 			array.add("Fronchak");
-			System.out.println(array.size());
+			System.out.println(array.toString());
+			System.out.println(array.exist("gabriel"));
+			array.add("Alice", 0);
+			System.out.println(array.toString());
+			array.add("Gmack", 1);
+			System.out.println(array.toString());
 		}
 		catch(DSException e) {
 			System.out.println("ERROR: " + e.getMessage());
--- src/dataStructures/Array.java
+++ src/dataStructures/Array.java
@@ -28,8 +28,66 @@ public class Array {
 		count++;
 	}
 	
+	public void add(String element, int position) {
+		if(!isPositionValid(position)) { throw new DSException("Invalid Position!"); }
+		if(isFull()) { throw new DSException ("Array is already full!"); }
+		if(position == count) { addElement(element); }
+		else {
+			moveForward(position);
+			this.elements[position] = element;
+			count++;
+		}
+	}
+	
+	private boolean isPositionValid(int position) {
+		return (position >= 0 && position <= count);
+	}
+	
+	private void moveForward(int position) {
+		if(this.elements[position + 1] != null) {
+			moveForward(position + 1);
+		}
+		this.elements[position + 1] = this.elements[position];
+		this.elements[position] = null;
+	}
+	
 	public int size() {
 		return count;
+	}
+	
+	@Override
+	public String toString() {
+		if(isEmpty()) { return ""; }
+		return getStringArray();
+	}
+	
+	public boolean isEmpty() {
+		return this.count == 0;
+	}
+	
+	private String getStringArray() {
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < size(); i++) {
+			sb.append(toStringElement(i));
+		}
+		return sb.toString();
+	}
+	
+	private String toStringElement(int i) {
+		return "Array[" + i + "] = " + this.elements[i] + "\n";
+	}
+	
+	public String get(int position) {
+		if(!isPositionValid(position)) {throw new DSException("Invalid Position!");}
+		if(this.elements[position] == null) {throw new DSException("Position is null!");}
+		return this.elements[position];
+	}
+	
+	public boolean exist(String element) {
+		for(int i = 0; i < count; i++) {
+			if(this.elements[i].equals(element)) { return true; }
+		}
+		return false;
 	}
 
 }
